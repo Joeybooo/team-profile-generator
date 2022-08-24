31d4b22a52d843d2183ae662f3f3e58785dd51f2
Joey Byrd <joeybyrd9999@gmail.com>
2022-08-24 15:52:29 -0700

    intern done

--- _tests_/Intern.test.js
+++ _tests_/Intern.test.js
@@ -2,5 +2,15 @@ const Intern = require('../lib/Intern');
 
 test('Inter Object', () => {
     const intern = new Intern('Joey', 50, 'joeybyrd9999@gmail.com', 'Berkley');
-    expect.(intern.school).toEqual(expect.any(String));
-})
+    expect(intern.school).toEqual(expect.any(String));
+});
+
+test('Employee School', () => {
+    const intern = new Intern('Joey', 50, 'joeybyrd9999@gmail.com', 'Berkley');
+    expect(intern.getSchool()).toEqual(expect.stringContaining(intern.school.toString()));
+});
+
+test('Role', () => {
+    const intern = new Intern('Joey', 50, 'joeybyrd9999@gmail.com', 'Berkley');
+    expect(intern.getRole()).toEqual("Intern");
+});

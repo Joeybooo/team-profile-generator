60405f9a5a32a3b9dc7c5fb3a0062c8bf0720ad8
Joey Byrd <joeybyrd9999@gmail.com>
2022-08-24 16:02:14 -0700

    added engineer html

--- src/htmlGen.js
+++ src/htmlGen.js
@@ -14,7 +14,7 @@ const GenerateManager = function (manager) {
         </div>
     </div>
     `; 
-}
+};
 
 const generateIntern = function (intern) {
     return `
@@ -33,3 +33,22 @@ const generateIntern = function (intern) {
 </div>
     `
 };
+
+const generateEngineer = function (engineer) {
+    return `
+    <div class="col-4 mt-4">
+        <div class="card h-100">
+            <div class="card-header">
+                <h3>${engineer.name}</h3>
+                <h4>Engineer</h4><i class="material-icons">laptop_mac</i>
+            </div>
+            <div class="card-body">
+                <p class="id">ID: ${engineer.id}</p>
+                <p class="email">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></p>
+                <p class="github">Github: <a href="https://github.com/${engineer.github}">${engineer.github}</a></p>
+            </div>
+        </div>
+    </div>
+    `
+};
+
